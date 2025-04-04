876b5208d21336135b8fe1cf58362cc0e6bdce86
HiC-BGOD <Blane3512@gmail.com>
2025-04-04 15:18:33 -0400

    done

--- arithmetic-service/src/main/java/edu/iu/p466/arithmetic_service/controller/ArithmeticController.java
+++ arithmetic-service/src/main/java/edu/iu/p466/arithmetic_service/controller/ArithmeticController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/add")
 public class ArithmeticController {
 
+
     IArithmeticService arithmeticService;
 
     public ArithmeticController(IArithmeticService arithmeticService)
